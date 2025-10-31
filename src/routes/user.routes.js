00b0d2b52dfc02fda0687887eeb6887ba5a33895
PRATHAM932004 <prathambhoraniya09@gmail.com>
2025-10-31 18:00:34 +0530

    Fix route path for refresh token endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,7 +36,7 @@ router.route("/login").post(loginUser);
 //Secured route
 router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("refresh-token").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
